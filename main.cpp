1e146eeb9a2f40a47ce2243181e42d6e88a544f5
JesseSipari <sipari.jesse@gmail.com>
2025-08-09 12:29:14 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+using std::vector;
+
+vector<vector<int>> initSquare(int square_size) {
+    vector<vector<int>> square(square_size, vector<int> (square_size, 0));
+    return square;
+}
+
+int getSquareSize() {
+    int square_size;
+    std::cout << "Insert the size of the square:\n";
+    std::cin >> square_size;
+    return square_size;
+}
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    int s_size;
+    s_size = getSquareSize();
+    initSquare(s_size);
+    std::cout << "size of the square is: " << s_size << "\n";
+    vector<vector<int>> square = initSquare(s_size);
+
+    return 0;
+}

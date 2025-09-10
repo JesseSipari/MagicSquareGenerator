ecee6b248da4f02f1c2312ef3e83f413fe290de1
JesseSipari <sipari.jesse@gmail.com>
2025-09-10 13:22:54 +0300

    feat: Add function to create doubly even cubes

--- main.cpp
+++ main.cpp
@@ -8,11 +8,26 @@ using std::vector;
 constexpr int MIN_SIZE = 3;
 constexpr int MAX_SIZE = 99;
 
-vector<vector<int>> initSquare(int square_size) {
+vector<vector<int>> initSquare(const int square_size) {
     vector<vector<int>> square(square_size, vector<int> (square_size, 0));
     return square;
 }
 
+void createDoublyEvenSquare(vector<vector<int>> &square, const int size) {
+    int num = 1;
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            square[row][col] = num++;
+        }
+    }
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (!((row % 4 == col % 4) || (row % 4 + col % 4 ==3 ))) {
+                square[row][col] = size * size - square[row][col] + 1;
+            }
+        }
+    }
+}
 
 void createOddSquare(vector<vector<int>> &sqr, const int size) {
     // Siamese method for odd squares
@@ -75,7 +90,12 @@ int main() {
     initSquare(s_size);
     std::cout << "size of the square is: " << s_size << "\n";
     vector<vector<int>> square = initSquare(s_size);
-    createOddSquare(square, s_size);
+    if (s_size % 2 != 0) {
+        createOddSquare(square, s_size);
+    }
+    if (s_size % 4 == 0) {
+        createDoublyEvenSquare(square, s_size);
+    }
     drawSquare(square, s_size);
 
 
